623a260b86ec106fdb13830427f64766d80b1269
artemgon <artemhoncharuk123@gmail.com>
2024-06-13 22:30:58 +0300

    Creating the first task of pe.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,35 @@
 #include <iostream>
-#include <ctime>
+#include <stdlib.h>
+#include <time.h> 
 using namespace std;
 
-int main() {
-	srand(time(0));
-	const int cols = 50;
-	const int rows = 50;
-	int array[cols][rows];
-	int current_cols = 0, current_rows = 0, sum = 0, max = 0, min = 0;
-	do
+int main()
+{
+	srand(time(NULL));
+	const int row = 3, col = 3;
+	int array[row][col], sum = 0, max = array[0][0];
+	for (int i = 0; i < row; i++)
 	{
-		cout << "Enter the number of columns: ";
-		cin >> current_cols;
-		cout << "Enter the number of rows: ";
-		cin >> current_rows;
-	} while (current_rows < 0 || current_rows > 50 || current_cols < 0 || current_cols > 50);
-	for (size_t i = 0; i < current_cols; i++)
-	{
-		for (size_t j = 0; j < current_rows; j++)
+		for (int j = 0; j < col; j++)
 		{
-			array[i][j] = rand() % 20 - 10;
-            sum = sum + array[i][j];
-            if (array[i][j] > max) {
-                max = array[i][j];
-            }
-            if (array[i][j] < min) {
-                min = array[i][j];
-            }
+			array[i][j] = rand() % 100;
+			sum += array[i][j];
+			cout << array[i][j] << " ";
+			if (array[i][j] > max) max = array[i][j];
+		}
+		cout << "\n\n";
+	}
+	int min = array[0][0];
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (array[i][j] < min) min = array[i][j];
 		}
 	}
-    for (size_t i = 0; i < current_cols; i++)
-    {
-        for (size_t j = 0; j < current_rows; j++)
-        {
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "Sum: " << sum;
-    cout << "\nArithmetic mean: " << sum / (current_cols * current_rows);
-    cout << "\nMin: " << min;
-    cout << "\nMax: " << max;
+	cout << "Sum = " << sum << endl;
+	cout << "Arithmetic mean = " << sum / (row * col) << endl;
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
 	return 0;
 }
