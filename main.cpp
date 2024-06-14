e7b3d3f61fdaa199bd86de56fe993674d1933364
artemgon <artemhoncharuk123@gmail.com>
2024-06-14 13:06:28 +0300

    Creating the first task of hw.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+	const int row = 5, col = 5;
+	int array[row][col], first_element;
+	cout << "Please, enter the first element: ";
+	cin >> first_element;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			array[i][j] = first_element;
+			cout << " " << array[i][j];
+			first_element *= 2;
+		}
+		cout << "\n\n";
+	}
 	return 0;
 }
