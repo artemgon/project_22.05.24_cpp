de74c5bf38f0a62e8f6fe66f57e73b7b470448b6
artemgon <artemhoncharuk123@gmail.com>
2024-06-14 12:24:28 +0300

    Creating the second task of pe.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int main() {
+	srand(time(NULL));
+	const int row = 3, col = 4;
+	int array[row][col],sum_3 = 0;
+	cout << endl;
+	for (int i = 0; i < row; i++)
+	{
+		int sum_1 = 0;
+		for (int j = 0; j < col; j++)
+		{
+			array[i][j] = rand() % 65;
+			cout << " " << array[i][j] ;
+			sum_1 += array[i][j];
+			sum_3 += array[i][j];
+		}
+		cout << "\t  |  " << sum_1 << endl;
+	}
+	cout << "-----------------------\n";
+	for (int j = 0; j < col; j++)
+	{
+		int sum_2 = 0;
+		for (int i = 0; i < row; i++)
+		{
+			sum_2 += array[i][j];
+		}
+		cout << " " << sum_2;
+		if (j == col - 1) cout << "\t |  " << sum_3;
+	}
 	return 0;
 }
